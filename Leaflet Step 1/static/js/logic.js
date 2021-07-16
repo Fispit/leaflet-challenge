543f3048c3e1ae794a34ea61a4ac83c4468eca31
Fispit <sdelcos@gmail.com>
2021-07-15 21:48:47 -0500

    Changes done to get a different way of getting the map working

--- Leaflet Step 1/static/js/logic.js
+++ Leaflet Step 1/static/js/logic.js
@@ -1,20 +1,39 @@
+var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 
 
 
-
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "dark-v10",
+  id: "light-v10",
   accessToken: API_KEY
 });
 
 
 // Create a baseMaps object
 var baseMaps = {
-  "Dark Map": darkmap
+  "Light Map": lightmap
 };
+
+
+//import geojson file
+varearthquakemarkers=[];
+
+
+
+
+
+d3.json(url).then((data)=>{
+
+ console.log(data)
+
+
+
+});
+
+
+
 
 // Create an overlay object
 var overlayMaps = {
@@ -25,7 +44,7 @@ var overlayMaps = {
 var myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5,
-  layers: [darkmap]
+  layers: [lightmap]
 });
 
 // Pass our map layers into our layer control
@@ -34,4 +53,3 @@ L.control.layers(baseMaps, {
   collapsed: true
 }).addTo(myMap);
 
-var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
